d18177175b6d9c5a11349b6808eaf68be63b8966
Priyesh Mehta <mehtapriyesh7401@gmail.com>
2020-03-30 22:32:54 +0530

    Update saavn_importer.py

--- saavn_importer.py
+++ saavn_importer.py
@@ -1,26 +1,23 @@
 #In this, I have used Beautiful to import a list of songs from Saavn.
 #I have used the playlist shown in the code for this Project.
+# -*- coding: utf-8 -*-
 """
 Created on Sat Nov  2 23:13:14 2019
 
 @author: user
 """
 
-from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
-driver = webdriver.Chrome()
+from urllib.request import urlopen
 
 songs=[] #List to store name of the song
+driver=urlopen("https://www.jiosaavn.com/s/playlist/4ba60ea5f92f9a23420dd219a7ea3914/Hindi_%F0%9F%96%A4/zt02fyOlt3Uwkg5tVhI3fw__")
+soup = BeautifulSoup(driver.read())
 
-driver.get("https://www.jiosaavn.com/s/playlist/4ba60ea5f92f9a23420dd219a7ea3914/Hindi_%F0%9F%96%A4/zt02fyOlt3Uwkg5tVhI3fw__")
-content = driver.page_source
-soup = BeautifulSoup(content)
 
-for a in soup.findAll('div',href=False, attrs={'itemprop':'track'}):
-    songs.append(a.find("meta", itemprop="name",content=True)["content"])
-
-songs=list(set(songs))
-df=pd.DataFrame(songs,columns=['Songs'])
-df.to_excel("C:\\Users\\user\\Documents\\songs.xlsx")
+for a in soup.findAll("p", {"class":"song-name ellip"}):
+    for n in a:
+        songs.append(n)
+   
 
